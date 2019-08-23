0b117f9ca47549c7166e8a595a960ce45cbc20b0
Dorothee Childs <childs@embl.de>
2019-08-23 22:30:59 +0200

    Add top-level package documentation

---
+++ R/NPARC.R
@@ -0,0 +1,12 @@
+#' \code{NPARC} package
+#'
+#' Non-parametric analysis of response curves
+#'
+#' See the preprint on
+#' \href{https://www.biorxiv.org/content/10.1101/373845v2}{Childs, Bach, Franken et al. (2019): Non-parametric analysis of thermal proteome profiles reveals novel drug-binding proteins}
+#'
+#' @docType package
+#' @name NPARC
+#' @importFrom rlang .data
+NULL
+
